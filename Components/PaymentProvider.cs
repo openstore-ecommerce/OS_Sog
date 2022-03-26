d559b1429aa17dcc299dcfb153a9a8f3b4ea0fa2
DNNMonster <coryg@storeoborealis.com>
2022-03-25 21:11:42 -0700

    Remove unused func

--- Components/PaymentProvider.cs
+++ Components/PaymentProvider.cs
@@ -37,16 +37,6 @@ namespace OS_Sog
             return templ;
         }
 
-        private Object ByteArrayToObject(byte[] arrBytes)
-        {
-            MemoryStream memStream = new MemoryStream();
-            BinaryFormatter binForm = new BinaryFormatter();
-            memStream.Write(arrBytes, 0, arrBytes.Length);
-            memStream.Seek(0, SeekOrigin.Begin);
-            Object obj = (Object)binForm.Deserialize(memStream);
-            return obj;
-        }
-
         public override string RedirectForPayment(OrderData orderData)
         {
             orderData.OrderStatus = "020";
